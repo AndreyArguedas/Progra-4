cf6553e70e48cb19c342bc7459f9a640a68a05f5
Andrey Arguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2017-03-25 20:51:01 -0600

    Studying

--- VI SEMANA/PracticaExamen/public_html/js/main.js
+++ VI SEMANA/PracticaExamen/public_html/js/main.js
@@ -1,1 +1,21 @@
 
+$(document).ready(function(){
+    $("#accionesImportantes").click(function(){
+        cargarJugadas();
+    });
+});
+
+function cargarJugadas(){
+    var text = '{"principalesAcciones":[{"minuto":"10","accion":"Remate a marco"},{"minuto":"30","accion":"Tarjeta Roja"}]}';
+    var json   = JSON.parse(text);
+    var html = "<table class='table table-hover'>";
+    html += "<thead><tr><td>Minuto</td><td>Accion</td></tr><thead>";
+    for(var i=0;i<json.principalesAcciones.length;i++){
+        html +="<tr>";
+        html+= "<td>"+json.principalesAcciones[i].minuto+"</td>";
+        html+= "<td>"+json.principalesAcciones[i].accion+"</td>";
+        html += "</tr>";
+    }
+    html += "</table>";
+    $("#mainRow").html(html);
+}
